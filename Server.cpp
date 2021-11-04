01ce777573c26d5d8a3a0a60e7556b7167d34a85
Sam Shannon <sshannon237@gmail.com>
2021-11-03 22:13:45 -0700

    Commented server.cpp as an outline

--- Server.cpp
+++ Server.cpp
@@ -7,8 +7,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include "ServerThread.cpp"
 static void *doit(void *);
 main() {
+
+    // set up socket to listen, then create a thread
     int sock, length;
     struct sockaddr_in server;
     int msgsock;
@@ -33,12 +36,25 @@ main() {
             perror("accept");
         }
         printf("before thread create\n");
-        pthread_create(&tid, NULL, doit, (void*) msgsock);
+        // Create thread
+        pthread_create(&tid, NULL, run, (void*) msgsock);
         printf("after thread create\n");
     }
-
 }
-static void * doit(void * arg) {
+
+// This is the run class. I think we can get away with just leaving this in Server.cpp 
+// instead of putting it in ServerThread.cpp(We would delete ServerThread.cpp in this case)
+
+// Finsihsed run method will look something like this:
+//
+// 1. open socket
+// 2. create httpresponce/httprequest classes which will take in the socket and have
+// methods for reading/writing to socket
+// 3. parse the beginning of the request to find out if get or post request
+// 4. call doGet or doPost
+static void * run(void * arg) {
+
+    // open socket
     DIR * dirp;
     struct direct *d;
     char buf1[80];
@@ -50,22 +66,32 @@ static void * doit(void * arg) {
     clientsock = (long long) (arg);
     printf("after assignment\n");
     /* pthread_detach(pthread_self());*/
+    // Reads socket - will be associated with HttpRequest class
     if ((rval = read(clientsock, buf1, 80)) < 0){
         perror("reading socket");
     }
+
+    // Prints HttpRequest. This will be logic that is in the doGet method.
     printf("%s\n",buf1);
+
+
+    // This opens a directory for responding to a client (doPost type things)
     dirp = opendir(buf1);
     if (dirp == NULL) {
         perror("openning dir");
         return (NULL);
     }
     buf2[0] = '\0';
+
+    // This writes to the socket. This will associated with HttpResponce class
     while (d = readdir(dirp)) {
         strcpy(buf2,d->d_name);
         if ((rval = write(clientsock, buf2, 1024)) < 0){
             perror("writing socket");
         }
     }
+
+    // close clientsocket
     closedir (dirp);
     close(clientsock);
     return (NULL);
